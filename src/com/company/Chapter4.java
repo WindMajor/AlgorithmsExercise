498f910f8333d70736abef89e3848ffe82499cfe
wangmeng <meng.wang@ihandysoft.com>
2017-05-09 21:37:45 +0800

    新增分治策略和矩阵乘法运算

--- src/com/company/Chapter4.java
+++ src/com/company/Chapter4.java
@@ -5,7 +5,19 @@ package com.company;
  */
 public class Chapter4 {
 
-    public static void findMaxCrossingSubarray(int[] A, int low, int mid, int high) {
+    public static class MaxArray {
+        int low;
+        int high;
+        int value;
+
+        public MaxArray(int low, int high, int value) {
+            this.low = low;
+            this.high = high;
+            this.value = value;
+        }
+    }
+
+    public static MaxArray findMaxCrossingSubarray(int[] A, int low, int mid, int high) {
         int leftSum = Integer.MIN_VALUE;
         int maxLeft = Integer.MIN_VALUE;
         int sum = 0;
@@ -28,6 +40,57 @@ public class Chapter4 {
                 maxRight = j;
             }
         }
-        System.out.println("maxLeft = " + maxLeft + " | leftSum = " + leftSum + " || maxRight = " + maxRight + " | rightSum = " + rightSum);
+        return new MaxArray(maxLeft, maxRight, leftSum + rightSum);
+    }
+
+    public static MaxArray findMaximumSubarray(int[] A, int low, int high) {
+
+        if (high == low) {
+            return new MaxArray(low, high, A[low]);
+        } else {
+            int mid = (low + high) / 2;
+
+
+            MaxArray leftArray = findMaximumSubarray(A, low, mid);
+            int leftLow = leftArray.low;
+            int leftHigh = leftArray.high;
+            int leftSum = leftArray.value;
+
+            MaxArray rightArray = findMaximumSubarray(A, mid + 1, high);
+            int rightLow = rightArray.low;
+            int rightHigh = rightArray.high;
+            int rightSum = rightArray.value;
+
+
+            MaxArray crossArray = findMaxCrossingSubarray(A, low, mid, high);
+            int crossLow = crossArray.low;
+            int crossHigh = crossArray.high;
+            int crossSum = crossArray.value;
+
+            if (leftSum >= rightSum && leftSum >= crossSum) {
+                return new MaxArray(leftLow, leftHigh, leftSum);
+
+            } else if (rightSum >= leftSum && rightSum >= crossSum) {
+                return new MaxArray(rightLow, rightHigh, rightSum);
+
+            } else {
+                return new MaxArray(crossLow, crossHigh, crossSum);
+            }
+        }
+    }
+
+    // 矩阵的乘法运算，C11 = A11 * B11 + A12 * B12;依次类推。
+    public static int[][] squareMatrixMultiply(int[][] A, int[][] B) {
+        int n = A.length;
+        int[][] C = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                C[i][j] = 0;
+                for (int k = 0; k < n; k++) {
+                    C[i][j] = C[i][j] + A[i][k] * B[k][j];
+                }
+            }
+        }
+        return C;
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,19 @@ public class Main {
     private static int[] B = new int[]{6, 7, 8, 9, 0, 1, 2, 3, 4, 5};
     private static int[] C = new int[]{13, -3, -25, 20, -3, -16, -23, 18, 28, -7, 12, -5, -22, 15, -4, 7};
 
+    private static int[][] AA = new int[][]{{2, 3}, {6, 7}};
+    private static int[][] BB = new int[][]{{4, 5}, {8, 9}};
+
     public static void main(String[] args) {
-        // write your code here
 
-        int[] ints = Chapter2.insertionSort_A(A);
-
+//        int[] result = Chapter2.insertionSort_A(A);
 //        for (int i = 0; i < ints.length; i++) {
-//            System.out.println("ints[" + i + "] = " + ints[i]);
+//            System.out.println("result[" + i + "] = " + ints[i]);
+//        }
+
+//        int[] result = Chapter2.merge(B, 0, 4, 9);
+//        for (int i = 0; i < result.length; i++) {
+//            System.out.println("result[" + i + "] = " + result[i]);
 //        }
 
 //        int[] result = Chapter2.mergeSort(A, 0, 0);
@@ -20,6 +26,17 @@ public class Main {
 //            System.out.println("result[" + i + "] = " + result[i]);
 //        }
 
-        Chapter4.findMaxCrossingSubarray(C, 0, 1, 15);
+//        Chapter4.MaxArray maxArray = Chapter4.findMaxCrossingSubarray(C, 0, 1, 15);
+//        System.out.println("maxLeft = " + maxArray.low + " maxRight = " + maxArray.high + " sum = " + maxArray.value);
+
+//        Chapter4.MaxArray maxArray = Chapter4.findMaximumSubarray(C, 0, 15);
+//        System.out.println("maxLeft = " + maxArray.low + " maxRight = " + maxArray.high + " sum = " + maxArray.value);
+
+        int[][] CC = Chapter4.squareMatrixMultiply(AA, BB);
+        for (int i = 0; i < CC.length; i++) {
+            for (int j = 0; j < CC[i].length; j++) {
+                System.out.println("CC[" + i + "][" + j + "] = " + CC[i][j]);
+            }
+        }
     }
 }
